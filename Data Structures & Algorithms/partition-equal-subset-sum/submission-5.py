7e65cfc1ba336c7528edb8927f7475635404fb54
caza2 <antoine.cazanave@polytechnique.edu>
2026-08-10 14:14:26 +0200

    Add: partition-equal-subset-sum - submission-5

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        S: int = 0
+        for num in nums:
+            S += num
+        if S % 2:
+            return False
+        inpt: list[int] = nums + [0]
+        cache: dict[str, int] = {str(inpt): S}
+
+        def f(l: list[int], index: int) -> Optional[bool]:
+            if len(l) == 0:
+                return
+            copy: list[int] = l.copy()
+            copy.pop(index)
+            if str(copy) not in cache:
+                cache[str(copy)] = cache[str(l)] - l[index]
+            if cache[str(copy)] == S/2:
+                return True
+            elif cache[str(copy)] < S/2:
+                return
+            else:
+                for i in range(len(copy)):
+                    if f(copy, i):
+                        return True
+        
+        if f(inpt, len(inpt)-1):
+            return True
+        return False
